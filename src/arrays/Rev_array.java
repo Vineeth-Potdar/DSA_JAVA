593f972c428838d2e16c156a156a7209909ccc2e
Vineeth-Potdar <vineeth.a.potdar@gmail.com>
2025-09-27 13:51:01 +0530

    Your comment

--- src/arrays/Rev_array.java
+++ src/arrays/Rev_array.java
@@ -4,7 +4,7 @@ public class Rev_array {
 	public static void main(String[] args) {
 		int[] arr = {1, 2, 3, 4};
 		System.out.println(Arrays.toString(arr));
-
+		int f = 0;
 		int start = 0 ;
 		int end = arr.length-1;
 		  
