3e87e43398af7abf1859c5f17def0d226cc874c8
Vineeth-Potdar <vineeth.a.potdar@gmail.com>
2025-10-08 14:53:34 +0530

    Update Binary_tree.java

--- src/day_8/Binary_tree.java
+++ src/day_8/Binary_tree.java
@@ -58,7 +58,7 @@ public class Binary_tree {
 		    }
 
 		    public static void main(String[] args) {
-		        int nodes[] = {1, 2, 4, 5, 7, 3, 9};
+		        int nodes[] = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 9, -1, -1};
 
 		        BinaryTree tree = new BinaryTree();
 		        Node root = tree.buildTree(nodes);
